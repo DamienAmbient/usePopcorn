ea9bddea84136206e4627f18d204db64ce84ec7b
Damien <damienambient@gmail.com>
2023-07-19 22:34:06 +0300

    first useEffect added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+    return <StarRating color="blue"></StarRating>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
+        {/* <StarRating maxRating={5}></StarRating>
+        <StarRating size={24} color="cyan" defaultRating={3}></StarRating>
+
+        <Test></Test> */}
     </React.StrictMode>
 );
